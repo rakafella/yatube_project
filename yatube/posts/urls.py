e438d3d41deb105e78fa6781bc7d7bcf2ea3cda7
Алексей Романов <rakafella25@gmail.com>
2022-07-09 15:24:34 +0300

    ???

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug/', views.group_posts),
-] 
+    path('', views.index, name = 'index_list'),
+    #path('group<slug:sl>/', views.group_posts, name = 'gr_list')
+    path('group/<slug:slug>/', views.group_posts, name = 'gr_list')
+]
+# Create your views here.
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,18 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
-    template = 'posts/index.html'
-    return render(request, template)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
-def group_posts(request):
-    template = 'posts/group_list.html'
-    return render(request, template)
-# Create your views here.
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group':group,
+        'posts':posts,
+    }
+    return render(request, 'posts/group_list.html', context)
