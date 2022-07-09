52d1bb5cdffaa7603f836d9a92e49c81363a52b0
Алексей Романов <rakafella25@gmail.com>
2022-07-09 14:53:05 +0300

    migrate

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     text = models.TextField()
@@ -11,5 +18,11 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
-# Create your models here.
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
